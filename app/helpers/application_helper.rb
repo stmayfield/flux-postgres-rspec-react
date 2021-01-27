54186bccd69ea64084bacb533135efca2955b26e
Stephen Mayfield <stmayfield@live.com>
2021-01-27 08:48:15 -0800

    added conditional title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def title_helper
+    if Rails.env.development?
+      "DEVELOPMENT - Flux Postgres Rspec React"
+    else
+      "Flux Postgres Rspec React"
+    end
+  end
 end
